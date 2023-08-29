918d726552afc4e2f887ac84e90a1c5c607c1ea1
Lisa <somilisa1010@gmail.com>
2023-08-29 23:22:43 +0100

    setup context, restaurant list and restuarant

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
+  const { restaurants, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (restaurants.length < 1) {
+    return (
+      <h2 className='section-title'>
+        {' '}
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+    <section>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
+        {restaurants.map((restaurant) => {
+          return <Cocktail key={restaurant.id} {...restaurant} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default CocktailList
+export default CocktailList;
--- src/context.js
+++ src/context.js
@@ -5,7 +5,44 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(false)
+  const [searchRestaurant, setSearchRestaurant] = useState('a');
+  const [searchState, setSearchState] = useState('');
+  const [searchLGA,setSearchLGA] = useState('');
+  const [restaurants, setRestaurants] = useState([]);
+
+  const fetchDrinks = async () =>{
+    setLoading(true)
+    try {
+      const response = await fetch(`${url}${searchRestaurant}`)
+      const data = await response.json();
+      const {drinks} = data;
+      if (drinks){
+        const newRestaurants = drinks.map(({idDrink,strDrink,strDrinkThumb, strAlcoholic, strGlass}) => 
+       { return {
+        id: idDrink,
+        name: strDrink,
+        image: strDrinkThumb,
+        info: strAlcoholic,
+        glass: strGlass,
+        }
+      })
+      setRestaurants(newRestaurants)
+      }
+      else{
+        setRestaurants([])
+      }
+      setLoading(false)
+    } catch (error) {
+      console.log('error');
+      setLoading(false)
+    }
+  }
+  useEffect(()=> {
+    fetchDrinks()
+  },[searchRestaurant])
+
+  return <AppContext.Provider value={{loading,searchRestaurant,searchState,searchLGA,restaurants}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
