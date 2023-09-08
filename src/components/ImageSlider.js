659343f474eff768ff73b34dacc1db60f5e336ef
Lisa <somilisa1010@gmail.com>
2023-09-08 11:41:12 +0100

    fix: carousel

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 import { cards } from '../data';
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 5,
+    items: 4,
     slidesToSlide: 5, // optional, default to 1.
+    partialVisibilityGutter: 40, // this is needed to tell the amount of px that should be visible.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 768 },
-    items: 4,
+    breakpoint: { max: 1024, min: 540 },
+    items: 3,
     slidesToSlide: 4, // optional, default to 1.
+    partialVisibilityGutter: 30, // this is needed to tell the amount of px that should be visible.
   },
   mobile: {
-    breakpoint: { max: 767, min: 464 },
-    items: 3,
+    breakpoint: { max: 540, min: 300 },
+    items: 1,
     slidesToSlide: 1, // optional, default to 1.
+    partialVisibilityGutter: 20, // this is needed to tell the amount of px that should be visible.
   },
 };
 const ImageSlider = () => {
@@ -36,7 +39,7 @@ const ImageSlider = () => {
       >
         {cards.map(({ imgUrl, id, text }) => {
           return (
-            <Link to='/groups'>
+            <Link to='/groups' className='group-card-link'>
               <div
                 className='group-card'
                 key={id}
